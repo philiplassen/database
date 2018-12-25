a3887d3c6c45e416d509b06b5478a019122684d1
Philip Lassen <philiplassen@gmail.com>
2018-12-25 11:53:48 +0100

    Simple io program for learning the basics of c++

---
+++ learningCplusplus/adder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a;
+  int b;
+  cout << "Please enter a number.\n";
+  cin >> a;
+  cout << "Please enter a number.\n";
+  cin >> b;
+  int sum = a + b;
+  cout << "The sum of those numbers is " << sum << endl;
+  return 0;
+}
